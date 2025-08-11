593c086dcf1decbea83afe7176540e796cb9deab
909meters <kuatkyzyaida@gmail.com>
2025-08-11 14:35:36 +0500

    Added Customer user model

--- hospital_system/users/models.py
+++ hospital_system/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        PATIENT = 'PATIENT', 'Patient'
+        DOCTOR = 'DOCTOR', 'Doctor'
+        ADMIN = 'ADMIN', 'Admin'
+
+    role = models.CharField(
+        max_length=50, 
+        choices=Role.choices,
+        default=Role.PATIENT,
+    )
